a742f4f7467436971c20ae5c23830722fd1d8437
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-04-02 19:28:01 -0700

    Added missing socket.js file

---
+++ src/public/js/socket.js
@@ -0,0 +1,19 @@
+var socket = new WebSocket("ws://" + location.host + "/scoresocket");
+
+socket.onmessage = function(event) {
+
+    var teams = $.parseJSON( event.data );
+
+    console.log(teams);
+
+    for (team in teams) {
+
+        if ($('#scoreboard tbody #' + team).length) {
+            $('#scoreboard tbody #' + team + ' #team-score').text(teams[team]);
+        } else {
+            $('#scoreboard tbody').append('<tr id="' + team + '"><td id="team-name">' + team + '</td><td id="team-score">' + teams[team] + '</td></tr>');
+        }
+
+    }
+
+}
