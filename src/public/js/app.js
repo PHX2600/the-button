478bc7a30fe48879a5ffa771fbcc424f2447c6f4
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-04-06 12:05:11 -0700

    Merged app.js and login.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
 
+
+    $("#loginbutton").click(function(event){
+
+        event.preventDefault();
+
+        $.ajax({
+            url: "/login",
+            type: "POST",
+            data: $("#loginform").serialize(),
+            statusCode: {
+                200: function() {
+                    window.location.replace("button");
+                },
+                403: function() {
+                    $("#failedlogin_message").hide();
+                    $("#failedlogin_message").show(500);
+                }
+            }
+        });
+
+    });
+
     $("#the-button-form").on('click', '#button', function(event) {
 
         event.preventDefault();
