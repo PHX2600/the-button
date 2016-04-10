da58ffd55f1b9576829b517e341b12df676a7693
AltF4 <altf4@phx2600.org>
2016-04-10 13:41:31 -0700

    There. Are you happy now, Chris?

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,17 @@
+function captcha() {
+  jQuery.get("/captcha", function(data){
+    var images = $.parseJSON( data );
+    $('#captcha').empty();
+    for (image in images) {
+      if (image == 1337){
+        $('#captcha').append("<input type='text' name='captcha_id' value='" + images[image] + "' id='captcha_id' style='display: none'>");
+      } else {
+        $('#captcha').append('<img src="/images/' + images[image] + '">');
+      }
+    }
+  });
+}
+
 $(document).ready(function() {
 
     $("#loginbutton").click(function(event){
@@ -17,20 +31,9 @@ $(document).ready(function() {
                 }
             }
         });
-
     });
 
-    jQuery.get("/captcha", function(data){
-        var images = $.parseJSON( data );
-        $('#captcha').empty();
-        for (image in images) {
-            if (image == 1337){
-                $('#captcha').append("<input type='text' name='captcha_id' value='" + images[image] + "' id='captcha_id' style='display: none'>");
-            } else {
-                $('#captcha').append('<img src="/images/' + images[image] + '">');
-            }
-        }
-    });
+    captcha();
 
     $("#the-button-form").on('click', '#button', function(event) {
 
@@ -40,17 +43,7 @@ $(document).ready(function() {
         var captcha_solve = $("#captcha-input").val();
         var captcha_id = $("#captcha_id").val();
 
-        jQuery.get("/captcha", function(data){
-            var images = $.parseJSON( data );
-            $('#captcha').empty();
-            for (image in images) {
-                if (image == 1337){
-                    $('#captcha').append("<input type='text' name='captcha_id' value='" + images[image] + "' id='captcha_id' style='display: none'>");
-                } else {
-                    $('#captcha').append('<img src="/images/' + images[image] + '">');
-                }
-            }
-        });
+        captcha();
 
         jQuery.post({
             url: "/button",
